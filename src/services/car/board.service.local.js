d2c4185074990a09257b876366ce9ba12305476d
OferKoren <oferkoren2@gmail.com>
2024-11-07 18:47:36 +0200

    connect demo data

--- src/services/car/board.service.local.js
+++ src/services/car/board.service.local.js
@@ -1,19 +1,17 @@
-
-import { storageService } from '../async-storage.service'
-import { makeId } from '../util.service'
+import { storageService } from '../async-storage.service.js'
+import { loadFromStorage, makeId, saveToStorage } from '../util.service.js'
 import { userService } from '../user'
-
+import { boardData } from '../../../data/board.js'
 const STORAGE_KEY = 'board'
-
+_createBoard()
 export const boardService = {
     query,
     getById,
     save,
     remove,
-    addBoardMsg
+    addBoardMsg,
 }
 window.cs = boardService
-
 
 async function query(filterBy = { txt: '' }) {
     var boards = await storageService.query(STORAGE_KEY)
@@ -21,14 +19,14 @@ async function query(filterBy = { txt: '' }) {
 
     if (txt) {
         const regex = new RegExp(filterBy.txt, 'i')
-        boards = boards.filter(board => regex.test(board.vendor) || regex.test(board.description))
+        boards = boards.filter((board) => regex.test(board.vendor) || regex.test(board.description))
     }
     // if(sortField === 'vendor' || sortField === 'owner'){
-    //     boards.sort((board1, board2) => 
+    //     boards.sort((board1, board2) =>
     //         board1[sortField].localeCompare(board2[sortField]) * +sortDir)
     // }
     // if(sortField === 'price' || sortField === 'speed'){
-    //     boards.sort((board1, board2) => 
+    //     boards.sort((board1, board2) =>
     //         (board1[sortField] - board2[sortField]) * +sortDir)
     // }
 
@@ -57,7 +55,7 @@ async function save(board) {
             vendor: board.vendor,
             // Later, owner is set by the backend
             owner: userService.getLoggedinUser(),
-            msgs: []
+            msgs: [],
         }
         savedBoard = await storageService.post(STORAGE_KEY, boardToSave)
     }
@@ -71,10 +69,18 @@ async function addBoardMsg(boardId, txt) {
     const msg = {
         id: makeId(),
         by: userService.getLoggedinUser(),
-        txt
+        txt,
     }
     board.msgs.push(msg)
     await storageService.put(STORAGE_KEY, board)
 
     return msg
+}
+
+function _createBoard() {
+    let board = loadFromStorage(STORAGE_KEY)
+    if (!board || !board.length) {
+        board = boardData.board
+        saveToStorage(STORAGE_KEY, board)
+    }
 }
