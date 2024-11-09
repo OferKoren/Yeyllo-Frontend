03afedc028139332f2934944da7a1d6ce7b9ddaa
Gal Ben David <gal.benda3@gmail.com>
2024-11-09 14:19:31 +0200

    Enable due date removal

--- src/cmps/Dates.jsx
+++ src/cmps/Dates.jsx
@@ -1,16 +1,26 @@
 import { useState, useEffect, useRef } from 'react'
 
-export function Dates({ task, handleChange, setIsEditDates }) {
+export function Dates({ task, handleChange, setIsEditDates, setTask }) {
 
-    const dateInputRef = useRef(null)
+    function onRemoveDueDate() {
+        const { dueDate, status, ...updatedTask } = task
+        setTask(updatedTask)
+    }
 
     return (
-        <input value={task.dueDate || "2024-10-23"}
-            type="date"
-            id="dueDate"
-            className="task-dueDate"
-            name="dueDate"
-            onChange={handleChange}
-        ></input>
+        <div className="modal-option">
+            <div className="option-modal-header">
+                <h2>Dates</h2>
+                <i className="btn fa-solid fa-xmark" onClick={() => setIsEditDates(false)}></i>
+            </div>
+            <input value={task.dueDate || "2024-10-23"}
+                type="date"
+                id="dueDate"
+                className="task-dueDate"
+                name="dueDate"
+                onChange={handleChange}
+            ></input>
+            <button className="btn btn-clear" onClick={onRemoveDueDate}>Remove</button>
+        </div>
     )
 }
