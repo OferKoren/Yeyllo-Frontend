500e488d0661fcfb850d889467412bd4b0e6d28c
Gal Ben David <gal.benda3@gmail.com>
2024-11-12 01:22:36 +0200

    Change icon elements from img to svg

--- data/board.js
+++ data/board.js
@@ -122,7 +122,7 @@ let board = [
         createdBy: {
             _id: 'u101',
             fullname: 'Abi Abambi',
-            imgUrl: '/img/user/user-default.png',
+            imgUrl: '/img/user/gal.png',
         },
         style: {
             // backgroundSize: 'contain',
@@ -145,12 +145,12 @@ let board = [
             {
                 _id: 'u101',
                 fullname: 'Abi Abambi',
-                imgUrl: '/img/user/user-default.png',
+                imgUrl: '/img/user/gal.png',
             },
             {
                 _id: 'u102',
                 fullname: 'Josh Ga',
-                imgUrl: '/img/user/user-default.png',
+                imgUrl: '/img/user/user-gal.png',
             },
         ],
         groups: [
@@ -195,7 +195,7 @@ let board = [
                                 byMember: {
                                     _id: 'u101',
                                     fullname: 'Tal Tarablus',
-                                    imgUrl: '/img/user/user-default.png',
+                                    imgUrl: '/img/user/gal.png',
                                 },
                             },
                         ],
@@ -217,7 +217,7 @@ let board = [
                         byMember: {
                             _id: 'u101',
                             fullname: 'Tal Tarablus',
-                            imgUrl: '/img/user/user-default.png',
+                            imgUrl: '/img/user/gal.png',
                         },
                         style: {
                             backgroundColor: '#26de81',
@@ -235,7 +235,7 @@ let board = [
                 byMember: {
                     _id: 'u101',
                     fullname: 'Abi Abambi',
-                    imgUrl: '/img/user/user-default.png',
+                    imgUrl: '/img/user/gal.png',
                 },
                 group: {
                     id: 'g101',
@@ -259,7 +259,7 @@ let board = [
         createdBy: {
             _id: 'u101',
             fullname: 'Abi Abambi',
-            imgUrl: '/img/user/user-default.png',
+            imgUrl: '/img/user/gal.png',
         },
         style: {
             // backgroundSize: 'contain',
@@ -282,12 +282,12 @@ let board = [
             {
                 _id: 'u101',
                 fullname: 'Tal Taltal',
-                imgUrl: '/img/user/user-default.png',
+                imgUrl: '/img/user/gal.png',
             },
             {
                 _id: 'u102',
                 fullname: 'Josh Ga',
-                imgUrl: '/img/user/user-default.png',
+                imgUrl: '/img/user/gal.png',
             },
         ],
         groups: [
@@ -332,7 +332,7 @@ let board = [
                                 byMember: {
                                     _id: 'u101',
                                     fullname: 'Tal Tarablus',
-                                    imgUrl: '/img/user/user-default.png',
+                                    imgUrl: '/img/user/gal.png',
                                 },
                             },
                         ],
@@ -354,7 +354,7 @@ let board = [
                         byMember: {
                             _id: 'u101',
                             fullname: 'Tal Tarablus',
-                            imgUrl: '/img/user/user-default.png',
+                            imgUrl: '/img/user/gal.png',
                         },
                         style: {
                             backgroundColor: '#26de81',
@@ -372,7 +372,7 @@ let board = [
                 byMember: {
                     _id: 'u101',
                     fullname: 'Abi Abambi',
-                    imgUrl: '/img/user/user-default.png',
+                    imgUrl: '/img/user/gal.png',
                 },
                 group: {
                     id: 'g101',
@@ -396,7 +396,7 @@ let board = [
         createdBy: {
             _id: 'u101',
             fullname: 'Abi Abambi',
-            imgUrl: '/img/user/user-default.png',
+            imgUrl: '/img/user/gal.png',
         },
         style: {
             // backgroundSize: 'contain',
@@ -419,12 +419,12 @@ let board = [
             {
                 _id: 'u101',
                 fullname: 'Tal Taltal',
-                imgUrl: '/img/user/user-default.png',
+                imgUrl: '/img/user/gal.png',
             },
             {
                 _id: 'u102',
                 fullname: 'Josh Ga',
-                imgUrl: '/img/user/user-default.png',
+                imgUrl: '/img/user/gal.png',
             },
         ],
         groups: [
@@ -469,7 +469,7 @@ let board = [
                                 byMember: {
                                     _id: 'u101',
                                     fullname: 'Tal Tarablus',
-                                    imgUrl: '/img/user/user-default.png',
+                                    imgUrl: '/img/user/gal.png',
                                 },
                             },
                         ],
@@ -491,7 +491,7 @@ let board = [
                         byMember: {
                             _id: 'u101',
                             fullname: 'Tal Tarablus',
-                            imgUrl: '/img/user/user-default.png',
+                            imgUrl: '/img/user/gal.png',
                         },
                         style: {
                             backgroundColor: '#26de81',
@@ -509,7 +509,7 @@ let board = [
                 byMember: {
                     _id: 'u101',
                     fullname: 'Abi Abambi',
-                    imgUrl: '/img/user/user-default.png',
+                    imgUrl: '/img/user/gal.png',
                 },
                 group: {
                     id: 'g101',
@@ -533,7 +533,7 @@ let board = [
         createdBy: {
             _id: 'u101',
             fullname: 'Abi Abambi',
-            imgUrl: '/img/user/user-default.png',
+            imgUrl: '/img/user/gal.png',
         },
         style: {
             backgroundImage: 'url(https://res.cloudinary.com/dkckt1l7i/image/upload/v1731148744/gradiant-volcano_mherm3.svg)',
@@ -554,12 +554,12 @@ let board = [
             {
                 _id: 'u101',
                 fullname: 'Tal Taltal',
-                imgUrl: '/img/user/user-default.png',
+                imgUrl: '/img/user/gal.png',
             },
             {
                 _id: 'u102',
                 fullname: 'Josh Ga',
-                imgUrl: '/img/user/user-default.png',
+                imgUrl: '/img/user/gal.png',
             },
         ],
         groups: [
@@ -604,7 +604,7 @@ let board = [
                                 byMember: {
                                     _id: 'u101',
                                     fullname: 'Tal Tarablus',
-                                    imgUrl: '/img/user/user-default.png',
+                                    imgUrl: '/img/user/gal.png',
                                 },
                             },
                         ],
@@ -626,7 +626,7 @@ let board = [
                         byMember: {
                             _id: 'u101',
                             fullname: 'Tal Tarablus',
-                            imgUrl: '/img/user/user-default.png',
+                            imgUrl: '/img/user/gal.png',
                         },
                         style: {
                             backgroundColor: '#26de81',
@@ -644,7 +644,7 @@ let board = [
                 byMember: {
                     _id: 'u101',
                     fullname: 'Abi Abambi',
-                    imgUrl: '/img/user/user-default.png',
+                    imgUrl: '/img/user/gal.png',
                 },
                 group: {
                     id: 'g101',
@@ -671,7 +671,7 @@ const user = {
     fullname: 'Abi Abambi',
     username: 'abi@ababmi.com',
     password: 'aBambi123',
-    imgUrl: '/img/user/user-default.png',
+    imgUrl: '/img/user/gal.png',
     mentions: [
         {
             //optional
--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -295,6 +295,7 @@ export function TaskDetails() {
                             placeholder="Add a more detailed description..."
                             value={task.description || ''}
                             onChange={handleInfoChange}
+                            style={{ backgroundColor: task.description ? '#f5f4f4' : '#e7e9eb', paddingBlockEnd: task.description ? '0.5em' : '2.3em' }}
                         />
                     </div>
 
@@ -315,16 +316,21 @@ export function TaskDetails() {
                 <div className="task-options">
                     <div>
                         <button className={`btn btn-option btn-light ${openModal === "members" && 'active'}`} onClick={() => handleToggleModal('members')}>
-                            <img src="/img/icons/icon-members.svg" />
-                            Members
+                            <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M12.0254 3C9.25613 3 7.01123 5.23858 7.01123 8C7.01123 10.7614 9.25613 13 12.0254 13C14.7946 13 17.0395 10.7614 17.0395 8C17.0395 5.23858 14.7946 3 12.0254 3ZM9.01688 8C9.01688 9.65685 10.3638 11 12.0254 11C13.6869 11 15.0338 9.65685 15.0338 8C15.0338 6.34315 13.6869 5 12.0254 5C10.3638 5 9.01688 6.34315 9.01688 8Z" style={{ fill: openModal === "members" ? '#ffffff' : '#333333' }} />
+                                <path fillRule="evenodd" clipRule="evenodd" d="M12.0254 11C16.7803 11 20.6765 14.6667 21.0254 19.3194C20.8721 20.2721 20.0439 21 19.0452 21H18.9741C18.9741 21 18.9741 21 18.9741 21L5.0767 21C5.07671 21 5.0767 21 5.0767 21L5.00562 21C4.00691 21 3.1787 20.2721 3.02539 19.3193C3.37428 14.6667 7.27038 11 12.0254 11ZM5.0767 19H18.9741C18.4875 15.6077 15.5618 13 12.0254 13C8.48892 13 5.56331 15.6077 5.0767 19ZM19.0451 19.9769V20.0231C19.0452 20.0154 19.0452 20.0077 19.0452 20C19.0452 19.9923 19.0452 19.9846 19.0451 19.9769Z" style={{ fill: openModal === "members" ? '#ffffff' : '#333333' }} />
+                            </svg>
+                            <span>Members</span>
                         </button>
                         {openModal === "members" && renderMembersModal()}
                     </div>
 
                     <div>
                         <button className={`btn btn-option btn-light ${openModal === "labels" && 'active'}`} onClick={() => handleToggleModal('labels')}>
-                            <img src="/img/icons/icon-labels.svg" />
-                            Labels
+                            <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M13.1213 2.80762C12.3403 2.02657 11.0739 2.02657 10.2929 2.80762L3.92891 9.17158C1.19524 11.9052 1.19524 16.3374 3.92891 19.0711C6.66258 21.8047 11.0947 21.8047 13.8284 19.0711L20.1924 12.7071C20.9734 11.9261 20.9734 10.6597 20.1924 9.87869L13.1213 2.80762ZM18.7782 11.2929L11.7071 4.22183L5.34313 10.5858C3.39051 12.5384 3.39051 15.7042 5.34313 17.6569C7.29575 19.6095 10.4616 19.6095 12.4142 17.6569L18.7782 11.2929ZM10 14C10 14.5523 9.55228 15 9 15C8.44772 15 8 14.5523 8 14C8 13.4477 8.44772 13 9 13C9.55228 13 10 13.4477 10 14ZM12 14C12 15.6569 10.6569 17 9 17C7.34315 17 6 15.6569 6 14C6 12.3431 7.34315 11 9 11C10.6569 11 12 12.3431 12 14Z" style={{ fill: openModal === "labels" ? '#ffffff' : '#333333' }} />
+                            </svg>
+                            <span>Labels</span>
                         </button>
                         {openModal === "labels" && renderLabelsModal()}
                     </div>
@@ -333,8 +339,10 @@ export function TaskDetails() {
                         <button
                             className={`btn btn-option btn-light ${openModal === "checklist" && 'active'}`}
                             onClick={() => handleToggleModal('checklist')}>
-                            <img src="/img/icons/icon-checklist.svg" />
-                            Checklist
+                            <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M6 4C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V13C20 12.4477 19.5523 12 19 12C18.4477 12 18 12.4477 18 13V18H6V6L16 6C16.5523 6 17 5.55228 17 5C17 4.44772 16.5523 4 16 4H6ZM8.73534 10.3223C8.36105 9.91618 7.72841 9.89038 7.3223 10.2647C6.91619 10.639 6.89039 11.2716 7.26467 11.6777L10.8768 15.597C11.4143 16.1231 12.2145 16.1231 12.7111 15.6264L13.0754 15.2683C13.3699 14.9785 13.6981 14.6556 14.0516 14.3075C15.0614 13.313 16.0713 12.3169 17.014 11.3848L17.0543 11.3449C18.7291 9.68869 20.0004 8.42365 20.712 7.70223C21.0998 7.30904 21.0954 6.67589 20.7022 6.28805C20.309 5.90022 19.6759 5.90457 19.2881 6.29777C18.5843 7.01131 17.3169 8.27244 15.648 9.92281L15.6077 9.96263C14.6662 10.8937 13.6572 11.8889 12.6483 12.8825L11.8329 13.6851L8.73534 10.3223Z" style={{ fill: openModal === "checklist" ? '#ffffff' : '#333333' }} />
+                            </svg>
+                            <span> Checklist</span>
                         </button>
                         {openModal === "checklist" && <AddChecklist setTask={setTask} handleCloseModal={handleCloseModal} />}
                     </div>
@@ -342,8 +350,11 @@ export function TaskDetails() {
                     <div>
                         <button className={`btn btn-option btn-light btn-date-picker ${openModal === "dates" && 'active'}`}
                             onClick={() => handleToggleModal('dates')}>
-                            <img src="/img/icons/icon-dates.svg" />
-                            Dates
+                            <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M13 6C13 5.44772 12.5523 5 12 5C11.4477 5 11 5.44772 11 6V12C11 12.2652 11.1054 12.5196 11.2929 12.7071L13.7929 15.2071C14.1834 15.5976 14.8166 15.5976 15.2071 15.2071C15.5976 14.8166 15.5976 14.1834 15.2071 13.7929L13 11.5858V6Z" style={{ fill: openModal === "dates" ? '#ffffff' : '#333333' }} />
+                                <path fillRule="evenodd" clipRule="evenodd" d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z" style={{ fill: openModal === "dates" ? '#ffffff' : '#333333' }} />
+                            </svg>
+                            <span>Dates</span>
                         </button>
                         {openModal === "dates" && renderDatesModal()}
                     </div>
@@ -351,8 +362,10 @@ export function TaskDetails() {
                     <div>
                         <button className={`btn btn-option btn-light ${openModal === "cover" && 'active'}`}
                             onClick={() => handleToggleModal('cover')}>
-                            <img src="/img/icons/icon-cover.svg" />
-                            Cover
+                            <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M5 5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5ZM19 7H5V13H19V7Z" style={{ fill: openModal === "cover" ? '#ffffff' : '#333333' }} />
+                            </svg>
+                            <span>Cover</span>
                         </button>
                         {openModal === "cover" && <Cover setTask={setTask} handleCloseModal={handleCloseModal} />}
                     </div>
@@ -362,8 +375,19 @@ export function TaskDetails() {
                     <div>
                         <button className={`btn btn-option btn-light`}
                             onClick={() => handleToggleModal('archive')}>
-                            <img src={`/img/icons/icon-${openModal === "archive" || openModal === "deleteTask" ? 'back-to-board' : 'archive'}.svg`} />
-                            {openModal === "archive" || openModal === "deleteTask" ? 'Send to board' : 'Archive'}
+                            {
+                                (openModal === "archive" || openModal === "deleteTask") ?
+                                    <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path fillRule="evenodd" clipRule="evenodd" d="M6.41147 14.6064C5.93123 14.8792 5.76301 15.4896 6.03574 15.9698C7.43928 18.4413 10.0483 20 12.9283 20C16.0259 20 18.799 18.1958 20.1021 15.4268C20.2136 15.1851 20.3641 14.7943 20.512 14.2831C20.9098 12.9079 21.0444 11.481 20.7475 10.1098C20.5471 9.18477 20.1545 8.33681 19.5479 7.59632C17.6868 5.32366 15.487 4 12.9283 4C10.0142 4 7.38651 5.59842 6 8.094V7.0003C6 6.44802 5.55228 6.0003 5 6.0003C4.44772 6.0003 4 6.44802 4 7.0003V10.9003C4 11.5047 4.48877 12.0003 5.099 12.0003H8.954C9.50628 12.0003 9.954 11.5526 9.954 11.0003C9.954 10.448 9.50628 10.0003 8.954 10.0003H7.32865C7.39632 9.80693 7.47457 9.61558 7.56276 9.42743C8.53951 7.35107 10.6143 6 12.9283 6C14.8015 6 16.4854 7.01323 18.0006 8.86357C18.3991 9.34999 18.6573 9.90771 18.7928 10.5332C19.0091 11.5322 18.9043 12.6434 18.5907 13.7274C18.473 14.1343 18.358 14.4332 18.2893 14.5821C17.3156 16.6511 15.2422 18 12.9283 18C10.7768 18 8.82724 16.8353 7.77486 14.9822C7.50213 14.5019 6.89172 14.3337 6.41147 14.6064Z" fill="currentColor" />
+                                    </svg>
+                                    :
+                                    <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M3.03418 5.59621C2.98604 5.04603 3.39303 4.56099 3.94322 4.51286L19.8823 3.11837C20.4325 3.07023 20.9175 3.47722 20.9657 4.02741L21.0528 5.0236L3.12133 6.5924L3.03418 5.59621Z" fill="currentColor" />
+                                        <path d="M9 12.9999C9 12.4476 9.44772 11.9999 10 11.9999H14C14.5523 11.9999 15 12.4476 15 12.9999C15 13.5522 14.5523 13.9999 14 13.9999H10C9.44772 13.9999 9 13.5522 9 12.9999Z" fill="currentColor" />
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M3 18.9999V7.99993H21V18.9999C21 20.1045 20.1046 20.9999 19 20.9999H5C3.89543 20.9999 3 20.1045 3 18.9999ZM5 9.99993H19V18.9999H5L5 9.99993Z" fill="currentColor" />
+                                    </svg>
+                            }
+                            <span> {openModal === "archive" || openModal === "deleteTask" ? 'Send to board' : 'Archive'}</span>
                         </button>
                     </div>
 
@@ -372,8 +396,10 @@ export function TaskDetails() {
                             <>
                                 <div>
                                     <button className={`btn btn-option btn-delete`} onClick={() => handleOpenModal('deleteTask')}>
-                                        <img src="/img/icons/icon-minus.svg" />
-                                        Delete
+                                        <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="3" y="11" width="18" height="2" rx="1" style={{ fill: '#ffffff' }} />
+                                        </svg>
+                                        <span> Delete</span>
                                     </button>
                                 </div>
                             </>
