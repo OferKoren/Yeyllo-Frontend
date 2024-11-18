c0c4324be62b32746bada7f093ba713ab6b36b11
matan-ode <matanodentz@gmail.com>
2024-11-18 17:54:07 +0200

    Update collapse functionality

--- src/cmps/GroupList.jsx
+++ src/cmps/GroupList.jsx
@@ -87,7 +87,7 @@ export function GroupList({ placeholder, onUpdateBoard, board }) {
                         {(provided, snapshot) => (
                             <div className="group" {...provided.dragHandleProps} {...provided.draggableProps} ref={provided.innerRef}>
                                 <li
-                                    style={{ ...group.style, rotate: snapshot.isDragging ? '5deg' : '', opacity: snapshot.isDragging ? '0.5' : '' }}
+                                    style={{ ...group.style , rotate: snapshot.isDragging ? '5deg' : '', opacity: snapshot.isDragging ? '0.5' : '' }}
                                     className={group.isCollapse ? `collapse ${group.id} ${group?.style?.backgroundColor?.substr(1)}` : group.id}
                                     key={group.id}
                                 >
--- src/cmps/GroupPreview.jsx
+++ src/cmps/GroupPreview.jsx
@@ -126,6 +126,7 @@ export function GroupPreview({ isModalOpen, setIsModalOpen, isLabelsClicked, set
 
             group.isCollapse = group.isCollapse ? false : true
             await onUpdateBoard(board)
+            // await updateBoardOptimistic(board)
         } catch (err) {
             console.log('err: ', err);
         } finally {
@@ -138,8 +139,8 @@ export function GroupPreview({ isModalOpen, setIsModalOpen, isLabelsClicked, set
             {
                 group.isCollapse
                     ?
-                    <article className={`group-preview collapse`} style={{/*padding:'0', width:'max-content', height:'100%'*/}}>
-                        <header style={{display:'flex', flexDirection:'column', }} className="group-header">
+                    <article className={`group-preview collapse`} style={{/*padding:'0', width:'max-content', height:'100%'*/ }}>
+                        <header style={{ display: 'flex', flexDirection: 'column', }} className="group-header">
                             <button style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} className={`group-menu-button collapse-btn ${group?.style?.backgroundColor?.substr(1)}`} onClick={onCollapse} ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="presentation"><path fill={`${group.style.backgroundColor ? colorLuminance(group.style.backgroundColor, -0.55) : 'currentcolor'}`} d="M18.062 11 16.5 9.914A1 1 0 1 1 17.914 8.5l2.616 2.616c.28.167.47.5.47.884s-.19.717-.47.884L17.914 15.5a1 1 0 0 1-1.414-1.414L18.062 13h-3.68c-.487 0-.882-.448-.882-1s.395-1 .882-1zM3.47 12.884c-.28-.167-.47-.5-.47-.884s.19-.717.47-.884L6.086 8.5A1 1 0 0 1 7.5 9.914L5.938 11h3.68c.487 0 .882.448.882 1s-.395 1-.882 1h-3.68L7.5 14.086A1 1 0 0 1 6.086 15.5z" /></svg></button>
                             <div onClick={onCollapse} className={`group-header-btn ${group?.style?.backgroundColor?.substr(1)}`}>{title}</div>
                         </header>
