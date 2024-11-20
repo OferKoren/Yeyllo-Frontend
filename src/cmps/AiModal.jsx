b0349e34f277ece40da1531bf96c87754c01c01d
Gal Ben David <gal.benda3@gmail.com>
2024-11-20 22:28:06 +0200

    Transfer the generating AI board logic to designated Ai modal component

--- src/cmps/AiModal.jsx
+++ src/cmps/AiModal.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+import axios from 'axios'
 
-export function AiModal() {
+export function AiModal({ onCloseModal }) {
 
     const [topic, setTopic] = useState('')
+    const navigate = useNavigate()
 
     function handleChange(ev) {
         const type = ev.target.type
@@ -20,10 +23,44 @@ export function AiModal() {
         setTopic(value)
     }
 
+    async function getPhotosArray(topic) {
+        const UNSPLASH_SEARCH_URL = 'https://api.unsplash.com/search/photos'
+        const UNSPLASH_ACCESS_KEY = 'anN0ohg_TPCWJd4ALToXR25XalJdkQBdFae7guKwQjE'
+
+        const response = await axios.get(UNSPLASH_SEARCH_URL, {
+            params: {
+                client_id: UNSPLASH_ACCESS_KEY,
+                query: topic,
+                page: 1,
+                per_page: 8,
+            },
+        })
+        const newPhotos = response.data.results
+
+        return newPhotos.map((photo) => photo.urls)
+    }
+
+    async function onGenerateAiBoard(ev) {
+        ev.preventDefault()
+        onCloseModal()
+        try {
+            const photos = await getPhotosArray(topic)
+            const generatedBoard = await boardService.generateAiBoard(topic, photos)
+            console.log('generatedBoard', generatedBoard)
+
+            setTimeout(() => {
+                navigate(`/board/${generatedBoard._id}`)
+            }, 3000)
+
+        } catch (err) {
+            console.error('can not generate board', err)
+        }
+    }
+
     return (
         <div className="ai-modal-container">
             <h3>Create AI Board</h3>
-            <form onSubmit={''}>
+            <form onSubmit={onGenerateAiBoard}>
                 <label htmlFor="topic">Name the project you'd like to create</label>
                 <input autoFocus className="ai-modal-input" type="text" id="topic" name="topic" value={topic} onChange={handleChange} />
                 <button style={{ backgroundColor: '#579dff' }}>Create</button>
